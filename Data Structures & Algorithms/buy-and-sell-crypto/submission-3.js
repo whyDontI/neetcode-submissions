661571204679b7d6c8fa00041ce6de0e54aa00ce
Nikhil Bhandarkar <nikhilbhandarkar1998@gmail.com>
2026-07-09 00:17:21 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let buy = 0;
+        let sell = 1;
+        let maxProfit = 0;
+
+        while (sell < prices.length) {
+            if (prices[buy] < prices[sell]) {
+                let profit = prices[sell] - prices[buy];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buy = sell;
+            }
+            sell++
+        }
+
+        return maxProfit;
+    }
+}
