9120868335cf4d4875d30e40fce91db73255e5a9
Nikhil Bhandarkar <nikhilbhandarkar1998@gmail.com>
2026-07-08 23:56:16 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let left = 0;
+        let right = height.length - 1;
+        let leftMax = 0;
+        let rightMax = 0;
+
+        let totalArea = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalArea += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalArea += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return totalArea;
+    }
+}
