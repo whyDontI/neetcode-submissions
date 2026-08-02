8a022abc03e1a385f7ba363956d82b01ae6eb481
Nikhil Bhandarkar <nikhilbhandarkar1998@gmail.com>
2026-08-02 16:24:44 +0530

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if (nums.length === 1) return nums[0];
+        const helper = (range) => {
+            if (range.length <= 0) return 0;
+            const memo = new Map();
+            
+            const dfs = (i) => {
+                if (i >= range.length) return 0;
+                if (memo.has(i)) return memo.get(i);
+
+                const result = range[i] + Math.max(dfs(i + 2), dfs(i + 3));
+                memo.set(i, result);
+                return result;
+            }
+
+            return Math.max(dfs(0), dfs(1));
+        }
+
+        // Exclude last house
+        const range1 = nums.slice(0, nums.length - 1);
+
+        // Exclude first house
+        const range2 = nums.slice(1);
+
+        return Math.max(helper(range1), helper(range2))
+    }
+}
