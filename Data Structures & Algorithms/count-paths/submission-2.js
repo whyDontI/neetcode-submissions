fd0cf2fdfdc21a108aa875c2a3f71913e46eff85
Nikhil Bhandarkar <nikhilbhandarkar1998@gmail.com>
2026-08-04 00:41:49 +0530

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+        const memo = Array(m).fill(0).map(() => Array(n).fill(0));
+
+        // Fill base case cells
+        for (let i = 0; i < m; i++) {
+            memo[i][n - 1] = 1;
+        }
+
+        for (let j = 0; j < n; j++) {
+            memo[m - 1][j] = 1;
+        }
+
+        for (let i = m - 2; i >= 0; i--) {
+            for (let j = n - 2; j >= 0; j--) {
+                memo[i][j] = memo[i + 1][j] + memo[i][j + 1];
+            }
+        }
+
+        return memo[0][0];
+    }
+}
