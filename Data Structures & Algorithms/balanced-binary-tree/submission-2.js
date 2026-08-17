4d7dcf4e063a993239a4027d73fb31eb504835d0
Nikhil Bhandarkar <nikhilbhandarkar1998@gmail.com>
2026-08-17 18:49:59 +0530

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        let isBalanced = true;
+
+        const heightOfTree = (node) => {
+            if (!node) return 0;
+            const left = heightOfTree(node.left);
+            const right = heightOfTree(node.right);
+
+            if (Math.abs(left - right) > 1 && isBalanced) {
+                isBalanced = false;
+            }
+
+            return 1 + Math.max(left, right);
+        }
+
+        heightOfTree(root);
+        return isBalanced;
+    }
+}
